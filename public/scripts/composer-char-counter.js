34149cf816281429c8583f5158d538a80230c609
GEDDE5 <geddes.3754@gmail.com>
2017-02-21 01:40:31 +0000

    Creates script to modify counter class to reflect chars remaining

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,6 @@
 $(document).ready(() => {
   $('.new-tweet textarea').on('keyup', function() {
-    let chars = $(this).val().length;
-    let charsLeft = 140 - chars;
+    let charsLeft = 140 - $(this).val().length;;
     let counter = $(this).closest('.new-tweet').find('.counter');
     counter.text(charsLeft);
     charsLeft < 0 ? counter.css('color', 'red') : counter.css('color', 'black');
