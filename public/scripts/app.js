8f8e9fe5ef9ea93754d4ffa7e0fe49472af66441
GEDDE5 <geddes.3754@gmail.com>
2017-02-22 01:02:20 +0000

    Adds functionality to refetch tweets on submission (note: merges new-tweet.js into app.js)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -42,5 +42,36 @@ $(document).ready( () => {
       renderTweets(tweets);
     });
   }
+
   loadTweets();
+
+  function submitHandler() {
+    $('.new-tweet input[type="submit"]').on('click', function (event) {
+      event.preventDefault();
+
+      let form = $(this).closest('form');
+      let input = form.find('textarea');
+      let error = form.find('p');
+
+      // removes error element if present
+      if(error.length) {
+        error.remove();
+      }
+
+      if(input.val() === '') {
+        $(this).after('<p>Error: Input cannot be empty</p>');
+      } else if(140 - input.val().length < 0) {
+        $(this).after('<p>Error: Input exceeds 140 characters</p>');
+      } else {
+        $.post('tweets', input.serialize(), function(tweet) {
+          $('.tweets').prepend(createTweetElement(tweet));
+          input.val('');
+        });
+      }
+
+    });
+  }
+
+  submitHandler();
+
 });
